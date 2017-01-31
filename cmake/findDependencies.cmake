240a69176129f8308c271b62a951130f693b870c
Luis Diaz Mas <piponazo@gmail.com>
2017-01-31 20:33:36 +0100

    Unify the cmake commands to lower-case

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -2,22 +2,22 @@
 #   Find Dependencies
 # ----------------------------------------------------------------------------
 
-FIND_PACKAGE(OpenCV 	REQUIRED )
+find_package(OpenCV 	REQUIRED )
 set (ARUCO_REQUIRED_LIBRARIES ${OpenCV_LIBS})
-INCLUDE_DIRECTORIES(${OpenCV_INCLUDE_DIRS})
+include_directories(${OpenCV_INCLUDE_DIRS})
 
 if(OpenCV_VERSION VERSION_LESS "3.0")#Opencv 2 requires the ad-hoc levmarq method for tracking since solvePnp from intrinsicguess does not work
-    IF(NOT USE_OWN_EIGEN3)
+    if(NOT USE_OWN_EIGEN3)
         find_package( Eigen3 REQUIRED )
-    ELSE()
+    else()
         set(EIGEN3_INCLUDE_DIR "3rdparty/eigen3")
-    ENDIF()
+    endif()
     include_directories( ${EIGEN3_INCLUDE_DIR} )
 
-    IF(USE_DOUBLE_PRECISION_PNP)
-        ADD_DEFINITIONS(-DDOUBLE_PRECISION_PNP)
-    ENDIF()
-ENDIF()
+    if(USE_DOUBLE_PRECISION_PNP)
+        add_definitions(-DDOUBLE_PRECISION_PNP)
+    endif()
+endif()
 
 
 
@@ -26,40 +26,40 @@ ENDIF()
 
 
 
-IF(EXISTS ${GLUT_PATH})
-    INCLUDE_DIRECTORIES(${GLUT_PATH}/include)
-    SET(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} ${GLUT_PATH}/lib)
-ENDIF()
+if(EXISTS ${GLUT_PATH})
+    include_directories(${GLUT_PATH}/include)
+    set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} ${GLUT_PATH}/lib)
+endif()
 
 ##LOOK FOR OPENGL AND GLUT
 #FIND OPENGL LIBRARY. In Android, there is no need since it is included
-IF(NOT ANDROID_CREATION)
+if(NOT ANDROID_CREATION)
 if(BUILD_GLSAMPLES)
   find_package(OpenGL)
   find_package(GLUT)#standard package
-  MESSAGE(STATUS "GLUT_FOUND=${GLUT_FOUND} OPENGL_gl_LIBRARY=${OPENGL_gl_LIBRARY} GLUT_HEADER=${GLUT_HEADER}")
+  message(STATUS "GLUT_FOUND=${GLUT_FOUND} OPENGL_gl_LIBRARY=${OPENGL_gl_LIBRARY} GLUT_HEADER=${GLUT_HEADER}")
 endif()
-  IF(NOT GLUT_FOUND) #else, freeglut
-    FIND_LIBRARY(GLUT_glut_LIBRARY     NAMES freeglut)
-    MESSAGE(STATUS "GLUT_glut_LIBRARY=${GLUT_glut_LIBRARY}")
-  ENDIF()
-  IF ( (NOT GLUT_glut_LIBRARY AND NOT GLUT_FOUND) OR NOT OPENGL_gl_LIBRARY)
+  if(NOT GLUT_FOUND) #else, freeglut
+    find_library(GLUT_glut_LIBRARY     NAMES freeglut)
+    message(STATUS "GLUT_glut_LIBRARY=${GLUT_glut_LIBRARY}")
+  endif()
+  if ( (NOT GLUT_glut_LIBRARY AND NOT GLUT_FOUND) OR NOT OPENGL_gl_LIBRARY)
     set(GL_FOUND "NO")
-  ELSE()
+  else()
     set(GL_FOUND "YES")
     set (OPENGL_LIBS  ${OPENGL_gl_LIBRARY} ${OPENGL_glu_LIBRARY} ${GLUT_glut_LIBRARY})
-  ENDIF()
-ENDIF()
+  endif()
+endif()
 #Omp
 
 
 if(WIN32)
  
-ELSE()
+else()
 find_package(OpenMP  )
 if (OPENMP_FOUND)
     set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
     set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
 add_definitions(-DUSE_OMP)
 endif()
-ENDIF()
+endif()
--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,9 +2,9 @@
 # Build type
 #------------------------------------------------------
 
-IF(NOT CMAKE_BUILD_TYPE )
-   SET( CMAKE_BUILD_TYPE "Debug" )
-ENDIF()
+if(NOT CMAKE_BUILD_TYPE )
+   set( CMAKE_BUILD_TYPE "Debug" )
+endif()
 
 #------------------------------------------------------
 # Lib Names and Dirs
@@ -13,45 +13,45 @@ ENDIF()
 if(WIN32)
     # Postfix of DLLs:
     # Postfix of DLLs:
-    SET(PROJECT_DLLVERSION "${PROJECT_VERSION_MAJOR}${PROJECT_VERSION_MINOR}${PROJECT_VERSION_PATCH}")
-    SET(RUNTIME_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin CACHE PATH "Directory for dlls and binaries")
-    SET(EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin CACHE PATH "Directory for binaries")
-    SET(LIBRARY_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin CACHE PATH "Directory for dlls")
+    set(PROJECT_DLLVERSION "${PROJECT_VERSION_MAJOR}${PROJECT_VERSION_MINOR}${PROJECT_VERSION_PATCH}")
+    set(RUNTIME_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin CACHE PATH "Directory for dlls and binaries")
+    set(EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin CACHE PATH "Directory for binaries")
+    set(LIBRARY_OUTPUT_PATH ${PROJECT_BINARY_DIR}/bin CACHE PATH "Directory for dlls")
 
 else()
     # Postfix of so's:
     set(PROJECT_DLLVERSION)
-#    SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_INSTALL_PREFIX}/lib/cmake/ /usr/lib/cmake)
+#    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_INSTALL_PREFIX}/lib/cmake/ /usr/lib/cmake)
 endif()
 
-OPTION(USE_OWN_EIGEN3	"Set to OFF to use a standard eigen3 version" ON)
-OPTION(USE_DOUBLE_PRECISION_PNP "Set Double/float precision for posetracker" ON)
-OPTION(BUILD_UTILS	"Set to OFF to not compile utils " ON)
-OPTION(BUILD_TESTS	"Set to OFF to not compile utils " OFF)
-OPTION(BUILD_SHARED_LIBS 	"Set to OFF to build static libraries" ON)
-OPTION(BUILD_GLSAMPLES 	"Set to OFF to build static libraries" OFF)
+option(USE_OWN_EIGEN3	"Set to OFF to use a standard eigen3 version" ON)
+option(USE_DOUBLE_PRECISION_PNP "Set Double/float precision for posetracker" ON)
+option(BUILD_UTILS	"Set to OFF to not compile utils " ON)
+option(BUILD_TESTS	"Set to OFF to not compile utils " OFF)
+option(BUILD_SHARED_LIBS 	"Set to OFF to build static libraries" ON)
+option(BUILD_GLSAMPLES 	"Set to OFF to build static libraries" OFF)
 
 # ----------------------------------------------------------------------------
 #   PROJECT CONFIGURATION
 #   force some variables that could be defined in the command line to be written to cache
 # ----------------------------------------------------------------------------
-OPTION(INSTALL_DOC 	"Set to ON to build/install Documentation" OFF)
-IF (INSTALL_DOC)
-    FIND_PACKAGE(Doxygen REQUIRED)
-    MESSAGE( STATUS "INSTALL_DOC:         ${INSTALL_DOC} ")
-    INCLUDE("${PROJECT_SOURCE_DIR}/generateDoc.cmake")
+option(INSTALL_DOC 	"Set to ON to build/install Documentation" OFF)
+if (INSTALL_DOC)
+    find_package(Doxygen REQUIRED)
+    message( STATUS "INSTALL_DOC:         ${INSTALL_DOC} ")
+    include("${PROJECT_SOURCE_DIR}/generateDoc.cmake")
     GENERATE_DOCUMENTATION(${PROJECT_SOURCE_DIR}/dox.in)
-ENDIF()
+endif()
 
 # ----------------------------------------------------------------------------
 #   Uninstall target, for "make uninstall"
 # ----------------------------------------------------------------------------
-CONFIGURE_FILE("${PROJECT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in" "${PROJECT_BINARY_DIR}/cmake_uninstall.cmake" IMMEDIATE @ONLY)
-ADD_CUSTOM_TARGET(uninstall "${CMAKE_COMMAND}" -P "${PROJECT_BINARY_DIR}/cmake_uninstall.cmake")
+configure_file("${PROJECT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in" "${PROJECT_BINARY_DIR}/cmake_uninstall.cmake" IMMEDIATE @ONLY)
+add_custom_target(uninstall "${CMAKE_COMMAND}" -P "${PROJECT_BINARY_DIR}/cmake_uninstall.cmake")
 
 # ----------------------------------------------------------------------------
 # create configuration file from .in file (If you use windows take care with paths)
 # ----------------------------------------------------------------------------
 
-CONFIGURE_FILE("${PROJECT_SOURCE_DIR}/config.cmake.in" "${PROJECT_BINARY_DIR}/Find${PROJECT_NAME}.cmake")
-INSTALL(FILES "${PROJECT_BINARY_DIR}/Find${PROJECT_NAME}.cmake" DESTINATION lib/cmake/ )
+configure_file("${PROJECT_SOURCE_DIR}/config.cmake.in" "${PROJECT_BINARY_DIR}/Find${PROJECT_NAME}.cmake")
+install(FILES "${PROJECT_BINARY_DIR}/Find${PROJECT_NAME}.cmake" DESTINATION lib/cmake/ )
