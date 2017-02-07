6ab83fe27efbbd91d04c25dcb2256b2ab9b14b69
Luis Díaz Más <piponazo@gmail.com>
2017-02-07 20:19:01 +0100

    Be able to compile the project with opencv2 and opencv3

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -2,13 +2,14 @@
 #   Find Dependencies
 # ----------------------------------------------------------------------------
 
-find_package(OpenCV REQUIRED core highgui imgcodecs imgproc calib3d features2d)
+find_package(OpenCV REQUIRED)
 include_directories( ${OpenCV_INCLUDE_DIRS} )
 
 message(STATUS "OpenCV found, version: ${OpenCV_VERSION} in dir ${OpenCV_INCLUDE_DIRS}")
 
 if(OpenCV_VERSION VERSION_LESS "3.0")#Opencv 2 requires the ad-hoc levmarq method for tracking since solvePnp from intrinsicguess does not work
 	if(NOT USE_OWN_EIGEN3)
+        message(WARNING "If you do not want to install Eigen you can turn on the option USE_OWN_EIGEN3")
 		find_package( Eigen3 REQUIRED )
 	else()
 		set(EIGEN3_INCLUDE_DIR "3rdparty/eigen3")
