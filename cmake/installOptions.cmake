c89c6d0e1565bfdac92b08c55f010e4571f6b8cf
Luis Diaz Mas <piponazo@gmail.com>
2017-02-21 18:09:59 +0100

    Provide auto-generated release configuration file

--- cmake/installOptions.cmake
+++ cmake/installOptions.cmake
@@ -1,6 +1,12 @@
 configure_file(cmake/arucoConfig.cmake.in arucoConfig.cmake IMMEDIATE @ONLY)
 install(FILES "${PROJECT_BINARY_DIR}/arucoConfig.cmake" DESTINATION share/aruco/cmake)
 
+if(CMAKE_BUILD_TYPE STREQUAL Release)
+    install(FILES "${PROJECT_SOURCE_DIR}/cmake/arucoConfig-release.cmake" DESTINATION share/aruco/cmake)
+else()
+    install(FILES "${PROJECT_SOURCE_DIR}/cmake/arucoConfig-debug.cmake" DESTINATION share/aruco/cmake)
+endif()
+
 # pkg-config
 configure_file(${PROJECT_SOURCE_DIR}/cmake/aruco.pc.in aruco.pc @ONLY)
 configure_file(${PROJECT_SOURCE_DIR}/cmake/aruco-uninstalled.pc.in aruco-uninstalled.pc @ONLY)
