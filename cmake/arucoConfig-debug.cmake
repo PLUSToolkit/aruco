9c454016eb330d7be0ed8dd51d76040b05f1e447
mark <mark.asselin@outlook.com>
2017-05-15 13:29:19 -0400

    re #1196: Enable building of ArUco on windows.
    Updated targets to allow building on Windows.

--- cmake/arucoConfig-debug.cmake
+++ cmake/arucoConfig-debug.cmake
@@ -7,14 +7,24 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 
 # Import target "aruco" for configuration "Debug"
 set_property(TARGET aruco APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
-set_target_properties(aruco PROPERTIES
-  IMPORTED_LINK_DEPENDENT_LIBRARIES_DEBUG "opencv_imgproc;opencv_calib3d;opencv_features2d"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libaruco.so.2.0.19"
-  IMPORTED_SONAME_DEBUG "libaruco.so.2.0"
-  )
 
-list(APPEND _IMPORT_CHECK_TARGETS aruco )
-list(APPEND _IMPORT_CHECK_FILES_FOR_aruco "${_IMPORT_PREFIX}/lib/libaruco.so.2.0.19" )
+if (WIN32)
+	set_target_properties(aruco PROPERTIES
+	  IMPORTED_LINK_DEPENDENT_LIBRARIES_DEBUG "opencv_imgproc;opencv_calib3d;opencv_features2d"
+	  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/aruco2019.lib"
+	  IMPORTED_SONAME_DEBUG "aruco2019.dll"
+	  )
+	list(APPEND _IMPORT_CHECK_TARGETS aruco )
+	list(APPEND _IMPORT_CHECK_FILES_FOR_aruco "${_IMPORT_PREFIX}/lib/aruco2019.lib" )
+else()
+	set_target_properties(aruco PROPERTIES
+	  IMPORTED_LINK_DEPENDENT_LIBRARIES_DEBUG "opencv_imgproc;opencv_calib3d;opencv_features2d"
+	  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libaruco.so.2.0.19"
+	  IMPORTED_SONAME_DEBUG "libaruco.so.2.0"
+	  )
+	list(APPEND _IMPORT_CHECK_TARGETS aruco )
+	list(APPEND _IMPORT_CHECK_FILES_FOR_aruco "${_IMPORT_PREFIX}/lib/libaruco.so.2.0.19" )
+endif()
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
--- cmake/arucoConfig-release.cmake
+++ cmake/arucoConfig-release.cmake
@@ -7,14 +7,23 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 
 # Import target "aruco" for configuration "Release"
 set_property(TARGET aruco APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(aruco PROPERTIES
-  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "opencv_imgproc;opencv_calib3d;opencv_features2d"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libaruco.so.2.0.19"
-  IMPORTED_SONAME_RELEASE "libaruco.so.2.0"
-  )
-
-list(APPEND _IMPORT_CHECK_TARGETS aruco )
-list(APPEND _IMPORT_CHECK_FILES_FOR_aruco "${_IMPORT_PREFIX}/lib/libaruco.so.2.0.19" )
+if (WIN32)
+	set_target_properties(aruco PROPERTIES
+	  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "opencv_imgproc;opencv_calib3d;opencv_features2d"
+	  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/aruco2019.lib"
+	  IMPORTED_SONAME_RELEASE "aruco2019.dll"
+	  )
+	list(APPEND _IMPORT_CHECK_TARGETS aruco )
+	list(APPEND _IMPORT_CHECK_FILES_FOR_aruco "${_IMPORT_PREFIX}/lib/aruco2019.lib" )
+else()
+	set_target_properties(aruco PROPERTIES
+	  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "opencv_imgproc;opencv_calib3d;opencv_features2d"
+	  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libaruco.so.2.0.19"
+	  IMPORTED_SONAME_RELEASE "libaruco.so.2.0"
+	  )
+	list(APPEND _IMPORT_CHECK_TARGETS aruco )
+	list(APPEND _IMPORT_CHECK_FILES_FOR_aruco "${_IMPORT_PREFIX}/lib/libaruco.so.2.0.19" )
+endif()
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
